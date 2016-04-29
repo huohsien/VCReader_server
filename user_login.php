3ae33cb543b544fc52ef52cbe663f0722baf83f2
Victor Jiang <huohsien@me.com>
2016-04-29 20:49:07 +0800

    block empty account name and password case that is caused by QQ sign up.

--- user_login.php
+++ user_login.php
@@ -4,10 +4,17 @@
     include_once("db.php");
     $account_name = $_REQUEST["account_name"];
     $account_password = $_REQUEST["account_password"];
+
+if (empty($account_name) || empty($account_password)) {
+        $response["error"]["code"] = '102';
+        $response["error"]["message"] = "Incorrect account name or password";
+        echo json_encode($response,true);
+        exit();
+    }
     $sql = "select * from User where account_name = '$account_name' and account_password  = '$account_password'";
     $list_r = db_q($sql);
     $rs = get_data($list_r);
-    if(empty($rs)) {
+    if (empty($rs)) {
         $response["error"]["code"] = '102';
         $response["error"]["message"] = "Incorrect account name or password";
     } else {
